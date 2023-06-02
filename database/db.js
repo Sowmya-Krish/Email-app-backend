fa5a0a45ef66728578c4562c8f6b90fc60e5a1cc
Sowmya-Krish <sowmikrish.crazy@gmail.com>
2023-06-02 18:31:54 +0530

    email app- backend

--- database/db.js
+++ database/db.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
-/* import dotenv from 'dotenv';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const USERNAME = process.env.DB_USERNAME;
-const PASSWORD = process.env.DB_PASSWORD;  */
+const PASSWORD = process.env.DB_PASSWORD;
 
 const Connection = () => {
-  const DB_URI = `mongodb+srv://sowmikrishcrazy:cF8IGSHesJxqhwJa@gmailclone.duaklpp.mongodb.net/?retryWrites=true&w=majority`;
+  const DB_URI = `mongodb+srv://${USERNAME}:${PASSWORD}@gmailclone.duaklpp.mongodb.net/?retryWrites=true&w=majority`;
   try {
     mongoose.connect(DB_URI, { useNewUrlParser: true });
     mongoose.set('strictQuery', false);
